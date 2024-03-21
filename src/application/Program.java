899a90b3396ec16dc16a744ad2d65cb415da1a28
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2024-03-21 16:08:43 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,10 +32,15 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		System.out.println("\n=== TEST 3: seller insert =====");
+		System.out.println("\n=== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg","greg@gmail.com",new Date(),4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = "+newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller Update =====");
+		seller = sellerDao.findById(11);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
